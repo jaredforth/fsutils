cfbf6082ca2088e9ae2ecf67c16461c5d2118e5f
Jared Forth <jaredforthdev@gmail.com>
2020-03-23 16:05:01 -0500

    update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,16 @@
-/// Collection of utility functions
+// Copyright 2020 Jared Forth.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Utilities for common filesystem operations
+//!
+//! fsutils provides an API based on Bash commands and includes a number
+//! of utility functions to make interacting with the filesystem simpler and more
+//! ergonomic.
 
 #[macro_use]
 extern crate log;
